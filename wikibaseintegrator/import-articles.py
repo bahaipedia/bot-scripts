9a9f15cce978144b6ff675b767a66790503fa592
David Haslip <103548776+davidhaslip@users.noreply.github.com>
2023-12-18 14:37:42 -0500

    Update import-articles.py
    Change expected input from editorial:true to "instanceof": "editorial" to accommodate other items than just editorial. Script checks to make sure the item referenced by instanceof exists and asks user to create it before processing the rest of the file if it doesn't.

--- wikibaseintegrator/import-articles.py
+++ wikibaseintegrator/import-articles.py
@@ -15,63 +15,73 @@ Usage: python import-articles.py Q224 (representing the Issue item where
 
 import json
 import sys
-import re
 from wikibaseintegrator import wbi_login, WikibaseIntegrator, wbi_helpers
 from wikibaseintegrator.datatypes import String, Item
-from wikibaseintegrator.datatypes.extra.localmedia import LocalMedia
 from wikibaseintegrator.wbi_config import config as wbi_config
 from wikibaseintegrator.wbi_enums import ActionIfExists
 
 # Configuration
 wbi_config['MEDIAWIKI_API_URL'] = 'https://bahaidata.org/api.php'
 wbi_config['USER_AGENT'] = 'MyWikibaseBot/1.0 (https://bahaidata.org/User:David)'
-login_instance = wbi_login.Clientlogin(user='David', password='hunter2')
+login_instance = wbi_login.Clientlogin(user='David', password='replaceme')
 
 # Initialize Wikibase Integrator
 wbi = WikibaseIntegrator(login=login_instance)
 
 def validate_json_format(file_name):
     with open(file_name, 'r', encoding='utf-8') as file:
-        try:
-            articles_data = json.load(file)
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON: {e}")
-            sys.exit(1)
+        articles_data = json.load(file)
 
-        for article in articles_data:
-            # Check for required keys
-            required_keys = ["title", "page_range"]
-            if not all(key in article for key in required_keys):
-                raise ValueError("Missing required keys in article entry.")
+    for article in articles_data:
+        # Check for required keys
+        required_keys = ["title", "page_range"]
+        if not all(key in article for key in required_keys):
+            raise ValueError("Missing required keys in article entry.")
 
-            # Check for author, editor, translator, or editorial
-            if ('author' not in article and 
-                'editor' not in article and 
-                'translator' not in article and 
-                'editorial' not in article):
-                raise ValueError(f"Article '{article['title']}' must have either an author, an editor, a translator, or be marked as editorial.")
+        # Check for author, editor, translator, or instanceof
+        if not any(key in article for key in ["author", "editor", "translator", "instanceof"]):
+            raise ValueError(f"Article '{article['title']}' must have either an author, an editor, a translator, or an 'instanceof' value.")
 
-            # Validate author format
-            if 'author' in article and not isinstance(article["author"], list):
-                raise ValueError(f"Author for '{article['title']}' is not formatted as a list.")
+        # Validate author format
+        if 'author' in article and not isinstance(article["author"], list):
+            raise ValueError(f"Author for '{article['title']}' is not formatted as a list.")
 
-            # Validate editorial format
-            if 'editorial' in article and not isinstance(article["editorial"], bool):
-                raise ValueError(f"Editorial field for '{article['title']}' is not a boolean.")
-
-            # Validate title and page range format
-            if not isinstance(article["title"], str) or not isinstance(article["page_range"], str):
-                raise ValueError(f"Title or Page Range for '{article['title']}' is not a string.")
-
-            # Validate editor format (expecting a single editor as a string)
-            if 'editor' in article and not isinstance(article["editor"], list):
-                raise ValueError(f"Editor for '{article['title']}' is not formatted as a list.")
+        # Validate editor format
+        if 'editor' in article and not isinstance(article["editor"], list):
+            raise ValueError(f"Editor for '{article['title']}' is not formatted as a list.")
                 
-            # Validate translator format
-            if 'translator' in article and not isinstance(article["translator"], list):
-                raise ValueError(f"Translator for '{article['title']}' is not formatted as a list.")
+        # Validate translator format
+        if 'translator' in article and not isinstance(article["translator"], list):
+            raise ValueError(f"Translator for '{article['title']}' is not formatted as a list.")
 
-        print("JSON file is valid.")
+        # Validate title and page range format
+        if not isinstance(article["title"], str) or not isinstance(article["page_range"], str):
+            raise ValueError(f"Title or Page Range for '{article['title']}' is not a string.")
+
+        # Validate instanceof format
+        if 'instanceof' in article and not isinstance(article["instanceof"], str):
+            raise ValueError(f"Instance of for '{article['title']}' is not formatted as a string.")
+
+    print("JSON file is valid.")
+        
+def handle_instanceof_items(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        articles_data = json.load(file)
+
+    for article in articles_data:
+        if 'instanceof' in article:
+            instance_type = article['instanceof']
+            search_result = wbi_helpers.search_entities(instance_type)
+            if not search_result:
+                print(f"'{instance_type}' does not exist.")
+                create_new = input(f"Would you like to create '{instance_type}'? (yes/no): ").strip().lower()
+                if create_new == "yes":
+                    create_new_item(instance_type)
+                else:
+                    print("Script terminated due to missing 'instanceof' item.")
+                    sys.exit(1)
+
+    print("All 'instanceof' items are valid or have been created.")
         
 def process_articles_from_file(file_name, magazine_issue_id):
     with open(file_name, 'r', encoding='utf-8') as file:
@@ -79,38 +89,41 @@ def process_articles_from_file(file_name, magazine_issue_id):
 
     for article in articles_data:
         title, page_range = article['title'], article['page_range']
-        person_item_ids = []
-        person_role = None
-        is_editorial = article.get('editorial', False)
+        person_item_ids = {
+            "author": [],
+            "editor": [],
+            "translator": []
+        }
+        instance_of_label = article.get('instanceof', None)
+        instance_of_value = None
+
+        if instance_of_label:
+            search_result = wbi_helpers.search_entities(instance_of_label)
+            instance_of_value = search_result[0]
 
         if 'author' in article:
             for author_name in article['author']:
                 person_item_id = check_or_create_author(author_name)
-                person_item_ids.append(person_item_id)
-            person_role = 'author'
-        elif 'editor' in article:
+                person_item_ids["author"].append(person_item_id)
+        if 'editor' in article:
             for editor_name in article['editor']:
                 person_item_id = check_or_create_editor(editor_name)
-                person_item_ids.append(person_item_id)
-            person_role = 'editor'
-        elif 'translator' in article:
+                person_item_ids["editor"].append(person_item_id)
+        if 'translator' in article:
             for translator_name in article['translator']:
                 person_item_id = check_or_create_translator(translator_name)
-                person_item_ids.append(person_item_id)
-            person_role = 'translator'
-        elif is_editorial:
-            # Handle unnamed editorial
-            person_role = 'editorial'
+                person_item_ids["translator"].append(person_item_id)
 
-        article_item_id = create_article_item(title, page_range, person_item_ids, magazine_issue_id, person_role)
+        article_item_id = create_article_item(title, page_range, person_item_ids, magazine_issue_id, instance_of_value)
 
-        for person_item_id in person_item_ids:
-            if person_role == 'author':
-                link_article_to_author(article_item_id, person_item_id)
-            elif person_role == 'editor':
-                link_article_to_editor(article_item_id, person_item_id)
-            elif person_role == 'translator':
-                link_article_to_translator(article_item_id, person_item_id)
+        for role, ids in person_item_ids.items():
+            for person_item_id in ids:
+                if role == 'author':
+                    link_article_to_author(article_item_id, person_item_id)
+                elif role == 'editor':
+                    link_article_to_editor(article_item_id, person_item_id)
+                elif role == 'translator':
+                    link_article_to_translator(article_item_id, person_item_id)
 
         link_article_to_magazine_issue(article_item_id, magazine_issue_id)
 
@@ -153,21 +166,24 @@ def check_or_create_translator(translator_name):
         print(f"Created translator {translator_name} ({new_translator_id})")
         return new_translator_id
         
-def create_article_item(title, page_range, person_item_ids, magazine_issue_id, person_role):
+def create_article_item(title, page_range, person_item_ids, magazine_issue_id, instance_of_value):
     article_item = wbi.item.new()
     article_item.labels.set(language='en', value=title)
     article_item.claims.add(Item(value=magazine_issue_id, prop_nr='P7'))
 
-    if person_role == 'author':
-        for person_item_id in person_item_ids:
+    # Use the instance_of_value directly, as it has already been validated
+    if instance_of_value:
+        article_item.claims.add(Item(value=instance_of_value, prop_nr='P12'))
+
+    if 'author' in person_item_ids:
+        for person_item_id in person_item_ids['author']:
             article_item.claims.add(Item(value=person_item_id, prop_nr='P10'), action_if_exists=ActionIfExists.APPEND_OR_REPLACE)
-    elif person_role == 'editor':
-        article_item.claims.add(Item(value=person_item_ids[0], prop_nr='P14')) 
-    elif person_role == 'translator':
-        article_item.claims.add(Item(value=person_item_ids[0], prop_nr='P32')) 
-    elif person_role == 'editorial':
-        editorial_id = 'Q19'
-        article_item.claims.add(Item(value=editorial_id, prop_nr='P12')) 
+    if 'editor' in person_item_ids:
+        for person_item_id in person_item_ids['editor']:
+            article_item.claims.add(Item(value=person_item_id, prop_nr='P14')) 
+    if 'translator' in person_item_ids:
+        for person_item_id in person_item_ids['translator']:
+            article_item.claims.add(Item(value=person_item_id, prop_nr='P32')) 
 
     article_item.claims.add(String(value=page_range, prop_nr='P6'))
     article_item.write()
@@ -197,15 +213,31 @@ def link_article_to_magazine_issue(article_item_id, magazine_issue_id):
     magazine_issue_item.claims.add(new_claim, action_if_exists=ActionIfExists.APPEND_OR_REPLACE)
     magazine_issue_item.write()
 
+def create_new_item(instance_type):
+    # Function to create a new item
+    new_item = wbi.item.new()
+    new_item.labels.set(language='en', value=instance_type)
+    new_item.write()
+    new_item_id = new_item.id
+    print(f"Created new item '{instance_type}' with ID {new_item_id}")
+    return new_item_id
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python add-articles-file.py <MagazineIssueID>")
+        print("Incorrect usage: python import-articles.py <MagazineIssueID>")
         sys.exit(1)
 
     magazine_issue_id = sys.argv[1]
     
-    # Validate JSON before processing
-    validate_json_format('import.json')
+    try:
+        # Attempt to validate JSON format
+        validate_json_format('import.json')
+    except json.JSONDecodeError as e:
+        print(f"Error processing JSON file, check syntax, also ensure the last item in the series does not have a comma. Error is: {e}")
+        sys.exit(1)
 
-    # Main processing
+    # Handle 'instanceof' items
+    handle_instanceof_items('import.json')
+
+    # Process articles
     process_articles_from_file('import.json', magazine_issue_id)
